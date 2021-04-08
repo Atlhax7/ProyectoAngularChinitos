2c0a6512ec09d49690054ae7cf586417d791a861
diego40g <diegopaznaula@hotmail.com>
2021-04-07 19:12:14 -0500

    change

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php
     const BASE_URL = "http://localhost/pos_mvc/";
     const HOST = "localhost";
-    const BD = "pos_mvc";
+    const BD = "gr14_comercializadora";
     const DB_USER = "root";
     const PASS = "";
     const CHARSET = "charset=utf8";
