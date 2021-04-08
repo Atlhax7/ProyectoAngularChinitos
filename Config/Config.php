9797c4d8d4a110ad045eefcb516cfc312667d49d
diego40g <diegopaznaula@hotmail.com>
2021-04-08 03:16:55 -0500

    this are much better

--- Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    const BASE_URL = "http://localhost/GR14_COMERCIALIZADORA/";
-    const HOST = "localhost";
-    const BD = "gr14_comercializadora";
-    const DB_USER = "root";
-    const PASS = "";
-    const CHARSET = "charset=utf8";
-?>
